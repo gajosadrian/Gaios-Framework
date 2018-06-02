49a62a6d3e2425862bf29655c4054a00b77f935d
Gajos <thegajos@gmail.com>
2018-06-02 16:27:20 +0200

    update

--- src/Lottery/Lottery.lua
+++ src/Lottery/Lottery.lua
@@ -15,8 +15,8 @@ function Lottery:addEntry(participant, lots)
     lots = lots or 1
 
     table.insert(self._entries, {
-        'participant' = participant,
-        'lots' = lots
+        participant = participant,
+        lots = lots
     })
 
     self._lotsTotal = self._lotsTotal + lots
