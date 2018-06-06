ccc79651d41bc754f51007f3eac69beeccd36c9e
Gajos <thegajos@gmail.com>
2018-06-06 12:06:33 +0200

    Update Player.lua

--- src/Auth/Player.lua
+++ src/Auth/Player.lua
@@ -65,7 +65,7 @@ local function getPlayers(mode)
     local players = {}
     for _, playerId in pairs(player(0, mode)) do
         local steamId = player(playerId, 'steamid')
-        table.insert(players, Auth.player(steamId))
+        table.insert(players, Config.user.model.find(steamId))
     end
     return players
 end
