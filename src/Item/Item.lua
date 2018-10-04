c8ca2b1e4a0726d96763f302920565a8748cd58a
BukC <fazekas.zsolt98@gmail.com>
2018-10-04 09:45:27 +0200

    Created Item Class and getters.

--- src/Item/Item.lua
+++ src/Item/Item.lua
@@ -1,6 +1,61 @@
 Item.Item = class()
 local Item = Item.Item
 
-function Item:constructor(id)
+function Item:constructor(id, x, y)
     self.id = id
+    self.x = x
+    self.y = y
+end
+
+Item.__index = function(self, key)
+    local id = rawget(self, 'id')
+    local x = rawget(self, 'x')
+    local y = rawget(self, 'y')
+
+    return switch(key) {
+        exists = function()
+            return item(id, 'exists')
+        end,
+
+        name = function()
+            return item(id, 'name')
+        end,
+
+        typeId = function()
+            return item(id, 'type')
+        end,
+
+        type = function()
+            return ItemType.new(id)
+        end,
+
+        player = function()
+            return item(id, 'player')
+        end,
+
+        ammo = function()
+            return item(id, 'ammo')
+        end,
+
+        ammoin = function()
+            return item(id, 'ammoin')
+        end,
+
+        mode = function()
+            return item(id, 'mode')
+        end,
+
+        dropped = function()
+            return item(id, 'dropped')
+        end,
+
+        droptimer = function()
+            return item(id, 'droptimer')
+        end,
+
+
+        [Default] = function()
+            return rawget(self, key)
+        end,
+    }
 end
