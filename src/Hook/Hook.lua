3f2a3d2e92ef9635e7b4100f5e02ccc3b8b9d97d
Gajos <thegajos@gmail.com>
2019-08-08 10:31:03 +0200

    Update Hook.lua

--- src/Hook/Hook.lua
+++ src/Hook/Hook.lua
@@ -2,6 +2,21 @@ local Hook
 Hook = class()
 
 Hook.LIST = {
+    always = {},
+    assist = {'player', 'player', 'player'},
+    attack = {'player'},
+    attack2 = {'player', 2},
+    bombdefuse = {'player'},
+    bombexplode = {'player', 2, 3},
+    bombplant = {'player', 2, 3},
+    break = {1, 2, 'playerOr0'},
+    build = {'player', 'objectType', 3, 4, 5, 'object'}, -- TODO: check if object_id is same everytime
+    buildattempt = {'player', 'objectType', 3, 4, 5},
+    buy = {'player', 'itemType'},
+    clientdata = {'player', 2, 3, 4},
+    clientsetting = {'player', 2, 3, 4},
+    collect = {'player', 'item', 4, 5, 6},
+
     join = {'player'},
 }
 
